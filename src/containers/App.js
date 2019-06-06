a20437b5ad9da3e46c6bd51ec2a7186c23d95684
Sara Ginsburg <Sara.ginsburg.ny@gmail.com>
2019-06-06 16:32:33 -0400

    added fetch request

--- src/containers/App.js
+++ src/containers/App.js
@@ -77,10 +77,19 @@ class App extends React.Component {
   state = {
     siblings: []
   }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/siblings')
+      .then(response => response.json())
+      .then(siblings => this.setState({ siblings }))
+  }
+
   render(){
+
+    console.log(this.state)
     return (
       <div className="App">
-        <Siblings siblings={siblings} />
+        <Siblings siblings={this.state.siblings} />
       </div>
     );
   }
