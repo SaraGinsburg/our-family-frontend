74fdac2919fcfd41dd7d9a3feb2805bf5bbe3a52
Sara Ginsburg <Sara.ginsburg.ny@gmail.com>
2019-06-06 15:56:44 -0400

    added styling to Siblings

--- src/containers/Siblings.js
+++ src/containers/Siblings.js
@@ -1,8 +1,17 @@
 import React from 'react';
-const Siblings = () => (
-  <div>
-  <h3>Siblings Component</h3>
-  </div>
-)
+
+import SiblingCard from '../components/SiblingCard'
+
+import './Siblings.css'
+
+const Siblings = ({siblings}) => {
+  const sibArray = siblings.map((sib) => <SiblingCard key={sib.id} sibling={sib}/>)
+  return (
+    <div className='SiblingsContainer'>
+    <h1>Siblings</h1>
+    {sibArray}
+    </div>
+  )
+}
 
 export default Siblings
