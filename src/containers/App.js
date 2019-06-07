fed2a14260514f87a81fb8e2d1aa88bbb4757ac9
Sara Ginsburg <Sara.ginsburg.ny@gmail.com>
2019-06-07 02:17:56 -0400

    modify fetch for testing

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,21 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Siblings from './Siblings';
 import './App.css';
-import Siblings from './Siblings';;
 
 class App extends React.Component {
-
   state = {
     siblings: []
   }
 
   componentDidMount() {
-    fetch('http://localhost:3000/api/v1/siblings')
+    fetch('http://localhost:3000/API/V1/siblings', {
+      mode: 'cors',
+        credentials: 'include'
+    })
       .then(response => response.json())
       .then(siblings => this.setState({ siblings }))
+    .catch(error => console.log('Authorization failed : ' + error.message));
+    // .then(siblings => console.log(siblings))
+      // .then(res => res.text())          // convert to plain text
+      // .then(text => console.log(text))  // then log it out
   }
 
-  render(){
-
+  render() {
     console.log(this.state)
     return (
       <div className="App">
@@ -24,6 +29,5 @@ class App extends React.Component {
     );
   }
 }
-
 
 export default App;
--- src/containers/Siblings.js
+++ src/containers/Siblings.js
@@ -1,17 +1,20 @@
 import React from 'react';
-
 import SiblingCard from '../components/SiblingCard'
-
 import './Siblings.css'
 
-const Siblings = ({siblings}) => {
-  const sibArray = siblings.map((sib) => <SiblingCard key={sib.id} sibling={sib}/>)
-  return (
-    <div className='SiblingsContainer'>
+
+const Siblings = (props) => (
+  <div>
     <h1>Siblings</h1>
-    {sibArray}
-    </div>
-  )
-}
+    {props.siblings.map(sibling =>
+      <div>
+        <h3>{sibling.attributes.first_name} {sibling.attributes.last_name}</h3>
+        <p> Birthdate: {sibling.attributes.birthdate}</p>
+        <p> Points Earned: {sibling.attributes.points_earned}</p>
+        <img className="SiblingImage" src={sibling.attributes.picture} alt="" />
+      </div>
+    )}
+  </div>
+)
 
 export default Siblings
